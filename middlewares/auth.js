f5ffe5b3f1348e17765ca0ded03b3b23f8e61a98
douglaspe <douglaspe15@gmail.com>
2020-06-27 22:38:55 -0300

    changing database uri

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -11,7 +11,7 @@ module.exports = async (req, res, next) => {
   const [scheme, token] = authHeader.split(' ');
 
   try {
-    const decoded = await promisify(jwt.verify)(token, 'secret');
+    const decoded = await promisify(jwt.verify)(token, process.env.SECRET);
 
     req.userId = decoded.id;
 
